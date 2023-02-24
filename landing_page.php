83c214501ff7f780ac262363ddfb679c4fe64e9b
santigc15 <gomezcacheiro@gmail.com>
2023-02-24 19:53:35 +0100

    landing_page

--- landing_page.php
+++ landing_page.php
@@ -4,13 +4,65 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/landing_style.css">
+
+
     <title>Landing page</title>
 </head>
+
 <body>
-    
+    <div class="container">
+        <header class="header">
+            <h1>FOTOGUAY</h1>
+            <div class="subtitle">Vota por la foto que más te guste</div>
+        </header>
+
+        <section class="images">
+            <div class="images-container">
+                <img src="./img_landing/foto01.jpg" alt="Imagen 1">
+                <div>
+                    <button>Votar ahora</button>
+                </div>
+            </div>
+            <div class="images-container">
+                <img src="./img_landing/foto02.jpg" alt="Imagen 2">
+                <div>
+                    <button>Votar ahora</button>
+                </div>
+            </div>
+            <div class="images-container">
+                <img src="./img_landing/foto03.jpg" alt="Imagen 3">
+                <div>
+                    <button>Votar ahora</button>
+                </div>
+            </div>
+            <div class="images-container">
+                <img src="./img_landing/foto04.jpg" alt="Imagen 4">
+                <div>
+                    <button>Votar ahora</button>
+                </div>
+            </div>
+            <div class="images-container">
+                <img src="./img_landing/foto05.jpg" alt="Imagen 5">
+                <div>
+                    <button>Votar ahora</button>
+                </div>
+            </div>
+            <div class="images-container">
+                <img src="./img_landing/foto06.jpg" alt="Imagen 6">
+                <div>
+                    <button>Votar ahora</button>
+                </div>
+            </div>
+        </section>
+    </div>
 </body>
+
+</body>
+
 </html>
